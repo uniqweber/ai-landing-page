9c56f39ab74fdff94fba29b13b261a5be8ccf0be
uniqweber <uniqwebersocial@gmail.com>
2025-03-06 14:56:27 +0600

    smooth scroll animation added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,24 @@
 import Footer from "../components/shared/Footer";
 import Benefits from "./homeComp/benefits/Benefits";
-import Collaboration from "./homeComp/collaoration/Collaboration";
+import Collaboration from "./homeComp/collaboration/Collaboration";
 import Hero from "./homeComp/hero/Hero";
 import Pricing from "./homeComp/pricing/Pricing";
 import Roadmap from "./homeComp/roadmap/Roadmap";
 import Services from "./homeComp/services/Service";
 
+import {useEffect} from "react";
+import Lenis from "lenis";
+
 const Home = () => {
+  useEffect(() => {
+    const lenis = new Lenis();
+    function update(time) {
+      lenis.raf(time);
+      requestAnimationFrame(update);
+    }
+    requestAnimationFrame(update);
+    return () => cancelAnimationFrame(update);
+  }, []);
   return (
     <>
       <Hero />
@@ -15,7 +27,7 @@ const Home = () => {
       <Services />
       <Pricing />
       <Roadmap />
-      <Footer/>
+      <Footer />
     </>
   );
 };
