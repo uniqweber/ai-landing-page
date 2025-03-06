a06f1620e7b4defc9da32edf180fe89884d265aa
uniqweber <uniqwebersocial@gmail.com>
2025-03-06 15:08:39 +0600

    semantic added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Button from "./components/UI/Button";
 import ButtonGradient from "./assets/images/svg/ButtonGradient";
 import Navbar from "./components/shared/Navbar";
 import Home from "./pages/Home";
@@ -7,10 +6,10 @@ import Home from "./pages/Home";
 const App = () => {
   return (
     <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+      <main className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Navbar />
         <Home />
-      </div>
+      </main>
       <ButtonGradient />
     </>
   );
